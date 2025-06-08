e31b5c94717b2a78a0bcf94d9de1fa98edf8bcb1
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-06-08 15:21:48 +0200

    Add fluent avalonia and show dialog

--- SampleWebView.Avalonia/MainWindow.xaml.cs
+++ SampleWebView.Avalonia/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using FluentAvalonia.UI.Controls;
 using WebViewControl;
 
 namespace SampleWebView.Avalonia {
@@ -11,6 +13,18 @@ namespace SampleWebView.Avalonia {
             AvaloniaXamlLoader.Load(this);
 
             DataContext = new MainWindowViewModel(this.FindControl<WebView>("webview"));
+        }
+
+        private async void ShowDialog_OnClick(object sender, RoutedEventArgs e) {
+            var dialog = new ContentDialog
+            {
+                Title = "My Dialog Title", 
+                PrimaryButtonText = "Ok", 
+                SecondaryButtonText = "Not OK", 
+                CloseButtonText = "Close",
+            };
+            
+            var result = await dialog.ShowAsync(this);
         }
     }
 }
